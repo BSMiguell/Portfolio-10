74f29163293af9c27710c53de3a9d72e3acbf62b
BSMiguell <bruno.s.miiguel@gmail.com>
2023-07-09 11:27:10 -0300

    Craindo HTML e o CSS e JS do Header

--- src/js/main.js
+++ src/js/main.js
@@ -11,8 +11,27 @@
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 
 /*==================== CHANGE BACKGROUND HEADER ====================*/
+function scrollHeader() {
+  const header = document.getElementById("header");
+
+  if (this.scrollY >= 80) header.classList.add("scroll-header");
+  else header.classList.remove("scroll-header");
+}
+
+window.addEventListener("scroll", scrollHeader);
 
 /*==================== SHOW SCROLL UP ====================*/
+function scrollUp() {
+  const scrollUp = document.getElementById("scroll-up");
+
+  if (window.scrollY >= 350) {
+    scrollUp.classList.add("show-scroll");
+  } else {
+    scrollUp.classList.remove("show-scroll");
+  }
+}
+
+window.addEventListener("scroll", scrollUp);
 
 /*==================== ABOUT TABS ====================*/
 const tabs = document.querySelectorAll("[data-target]");
